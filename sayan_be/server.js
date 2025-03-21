9d4049fd0a8cac29e2e7c6ad39190524ad61016a
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-03-22 00:30:59 +0530

    minor change

--- sayan_be/server.js
+++ sayan_be/server.js
@@ -32,9 +32,10 @@ const app = express();
 // Enable CORS with specific configuration for local testing
 // This allows requests from any origin during development
 app.use(cors({
-    origin: process.env.CLIENT_URL, // Allow all origins for testing
+    origin: ['https://sayan-fs-portfolio.vercel.app', 'http://localhost:3000'], // Allow Vercel domain and local development
     methods: ['GET', 'POST', 'DELETE'], // Allow necessary methods including DELETE
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true // Allow credentials
 }));
 
 // Middleware
